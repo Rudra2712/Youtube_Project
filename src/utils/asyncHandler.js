73094bc52a12f9938dd7eae77374e1ad9dfe885a
Rudra <rudramoradiya2712@gmail.com>
2025-05-26 14:57:01 +0530

    util code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const aysncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler (req,res,next)).catch((err)=>next(err));
+    }
+}
+
+export{aysncHandler}
+
+
+// const aysncHandler=(fn) =>async(req,res,next)=>{
+//     try{
+//         await fn(req,res,next);     
+//     }catch(err){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message :err.message
+//         })
+//     }
+// }
